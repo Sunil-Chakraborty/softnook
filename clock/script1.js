465e4d419d607fc3a594d4c2720ee00be1514c21
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-04-16 23:37:22 +0530

    Update script1.js

--- clock/script1.js
+++ clock/script1.js
@@ -30,7 +30,6 @@ displayTimeInTimeZone("Pacific/Honolulu", "hawaiiTime");
 displayTimeInTimeZone("America/Denver", "mountainTime");
 displayTimeInTimeZone("America/Los_Angeles", "pacificTime");
 
-
 // Function to update the IST time in the heading
 function updateISTTime() {
     const istHeading = document.getElementById('ist-heading');
@@ -38,7 +37,7 @@ function updateISTTime() {
     const options = { timeZone: 'Asia/Kolkata', hour12: true, hour: 'numeric', minute: '2-digit' };
     const istTime = now.toLocaleTimeString('en-US', options); // Format IST time
 
-    istHeading.textContent = `Time Zones Conversion from IST (${istTime})`; // Update heading with IST time
+    istHeading.textContent = `USA Time Zones Conversion from IST (${istTime})`; // Update heading with IST time
 }
 
 // Update IST time initially
@@ -47,4 +46,43 @@ updateISTTime();
 // Update IST time every second
 setInterval(updateISTTime, 1000);
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the audio player element
+    const audioPlayer = document.getElementById('audio-player');
 
+    // Add a click event listener to the document body
+    document.body.addEventListener('click', function() {
+        // Toggle playback of the audio player when the body is clicked
+        if (audioPlayer.paused) {
+            audioPlayer.play(); // If paused, play the audio
+			const modal = document.getElementById('modal');
+
+			
+			document.getElementById('closeModal').addEventListener('click', function() {
+				toggleModal();
+			});
+			
+        } else {
+            audioPlayer.pause(); // If playing, pause the audio
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal element
+    const modal = document.getElementById('modal');
+
+    // Function to toggle modal visibility
+    function toggleModal() {
+        modal.style.display = (modal.style.display === 'none' || modal.style.display === '') ? 'flex' : 'none';
+    }
+
+    // Add event listener to show modal when clicking outside the footer
+    document.body.addEventListener('click', function(event) {
+        if (!event.target.closest('#footer')) {
+            toggleModal();
+        }
+    });
+
+    
+});
